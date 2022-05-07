c61d580096334612facfa9a6041720a854906cac
Ashley Wong <ashleyltwong@gmail.com>
2022-05-07 16:47:46 +0800

    camera follows player as it moves, camera rotates with mouse

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
+    // target is the player
     public Transform target;
     public Vector3 offset;
     public bool useOffsetValues;
+    public float rotateSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +22,18 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // camera follows player around
-        transform.position = target.position - offset;
+        // rotate player based on mouse
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        // vertical applied to x and horizontal applied to y
+        target.Rotate(-vertical, horizontal, 0);
+
+        // rotate camera around player (and mouse) + camera follows player around
+        float desiredYAngle = target.eulerAngles.y;
+        float desiredXAngle = target.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+
+        transform.position = target.position - rotation * offset;
         transform.LookAt(target);
     }
 }
