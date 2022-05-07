92e6cd14c2f40337a2149c26ad008dd6a45bd013
Ashley Wong <ashleyltwong@gmail.com>
2022-05-07 15:59:29 +0800

    tutorial 3 done -- player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,22 +6,32 @@ public class PlayerController : MonoBehaviour
 {
 
     public float moveSpeed;
-    public Rigidbody theRB;
     public float jumpForce;
+    public CharacterController controller;
+
+    private Vector3 moveDirection;
+    public float gravityScale;
 
     // Start is called before the first frame update
     void Start()
     {
-        theRB = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
-
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
+        
+        // control jumping
         if (Input.GetButtonDown("Jump")) {
-            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
+            moveDirection.y = jumpForce;
         }
+
+        // gravity
+        moveDirection.y = moveDirection.y + Physics.gravity.y * gravityScale;
+
+        // update character controller
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
